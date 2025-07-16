95f98fb6249d910be8adeb35b1974e35ee0c8646
wholelottarido <eridhooo19@gmail.com>
2025-07-16 21:42:39 +0700

    Fix: Properly track client directory contents

--- client/src/pages/LaptopListPage.js
+++ client/src/pages/LaptopListPage.js
@@ -76,7 +76,8 @@ const LaptopListPage = () => {
     const fetchLaptops = async () => {
       setLoading(true);
       try {
-        const response = await axios.get('http://localhost:5001/api/get-laptops', {
+        const API_URL = process.env.REACT_APP_API_URL;
+        const response = await axios.get(`${API_URL}/api/get-laptops`, {
           params: { search: searchTerm, sort: sortBy }
         });
         setLaptops(response.data);
